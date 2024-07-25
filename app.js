f9ddf770024628266ff6fab53f40ba239f7adc16
Dimitri V <dvardidze15@gmail.com>
2024-07-25 13:49:30 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function markRead() {
+    document.getElementById('active').style.backgroundColor = 'transparent'
+    document.getElementById('active1').style.backgroundColor = 'transparent'
+    document.getElementById('active2').style.backgroundColor = 'transparent'
+    document.getElementById('dot').style.display = 'none'
+    document.getElementById('dot1').style.display = 'none'
+    document.getElementById('dot2').style.display = 'none'
+    document.getElementById('count').style.display = 'none'
+
+}
